a4776ef496498b8b37b25f30502863d9765c634d
Suman <dhalisuman001@gmail.com>
2022-08-12 20:07:49 +0530

    delete pretter and eslint cause of error and page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom'
+// import { Switch } from 'react-router'
 const App = () => {
   return (
-    <div>App</div>
+
+    <Switch>
+
+      <Route path="/" exact    > This is home page</Route>
+      <Route path="/about"     > This is about page</Route>
+      <Route > Not Found 404 error</Route>
+    </Switch>
+
   )
 }
 
-export default App
+export default App;
