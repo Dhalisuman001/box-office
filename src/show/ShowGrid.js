5e4c6f9476912246c5ed5f6a3704713962aabccd
Suman <dhalisuman001@gmail.com>
2022-08-13 12:03:14 +0530

    Starred page

--- src/show/ShowGrid.js
+++ src/show/ShowGrid.js
@@ -2,18 +2,37 @@ import React from 'react'
 import ShowCard from './ShowCard';
 import img from '../asset/not-found.png';
 import { FlexGrid } from '../components/Styled';
+import { useShows } from '../misc/custom-hooks';
 const ShowGrid = ({ data }) => {
+
+    const [starredShow, dispatchStarred] = useShows();
+
     return (
         <FlexGrid>
-            {data.map(({ show }) => (
-                <ShowCard
-                    key={show.id}
-                    id={show.id}
-                    name={show.name}
-                    image={show.image ? show.image.medium : img}
-                    summary={show.summary}
-                />
-            ))}
+            {data.map(({ show }) => {
+                const isStarred = starredShow.includes(show.id);
+                const onStarClick = () => {
+                    if (isStarred) {
+                        dispatchStarred({ type: 'REMOVE', showId: show.id })
+                    }
+                    else {
+                        dispatchStarred({ type: 'ADD', showId: show.id })
+                    }
+
+                }
+
+                return (
+                    <ShowCard
+                        key={show.id}
+                        id={show.id}
+                        name={show.name}
+                        image={show.image ? show.image.medium : img}
+                        summary={show.summary}
+                        onStarClick={onStarClick}
+                        isStarred={isStarred}
+                    />
+                );
+            })}
         </FlexGrid>
     )
 }
